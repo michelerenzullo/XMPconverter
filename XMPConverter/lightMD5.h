1459ef47d5c955808cc876fe6d774e78df8b2bd0
michelerenzullo <michele.renzullo98@gmail.com>
2021-03-17 20:14:58 +0100

    add cstring for linux

--- XMPConverter/lightMD5.h
+++ XMPConverter/lightMD5.h
@@ -1,14 +1,17 @@
 #ifndef LIGHTMD5_H
 #define LIGHTMD5_H
 #include <string>
+#if !defined(_WIN32) || !defined(__WIN32__)
+#include <cstring>
+#endif
 
-std::string md5_process(uint8_t *, size_t);
+std::string md5_process(uint8_t*, size_t);
 
-template <typename T> std::string md5(T initial_msg, size_t initial_len){ 
- if constexpr (std::is_same_v<T,std::string>)
-  return md5_process((uint8_t*)initial_msg.c_str(),initial_len);
- else
-  return md5_process((uint8_t*)initial_msg,initial_len);
+template <typename T> std::string md5(T initial_msg, size_t initial_len) {
+	if constexpr (std::is_same_v<T, std::string>)
+		return md5_process((uint8_t*)initial_msg.c_str(), initial_len);
+	else
+		return md5_process((uint8_t*)initial_msg, initial_len);
 }
 
 #endif
