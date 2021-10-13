b7cd4539f7144e5205c665ab7e849c9b7c13e0a3
michelerenzullo <76132686+michelerenzullo@users.noreply.github.com>
2021-10-13 19:01:13 +0200

    fixed memory leak when reading cube data points

--- XMPConverter/XMPconverter.cpp
+++ XMPConverter/XMPconverter.cpp
@@ -187,9 +187,10 @@ void encode(string path, string& outFileName) {
 	printf("- test encoding back -\nTITLE: %s\nSIZE: %d\n", options.title.c_str(), input_size);
 	double* samples_1 = new double[input_size * input_size * input_size * 3];
 
-	char* points = nullptr;
-	for (const char* s = text->c_str(); *s;) if (*s++ == '\n' && *s <= '9' && *s >= '0') { points = _strdup(s); delete text;  break; }
+	char* points = nullptr; char* points_address = nullptr;
+	for (const char* s = text->c_str(); *s;) if (*s++ == '\n' && *s <= '9' && *s >= '0') { points = points_address = _strdup(s); delete text;  break; }
 	for (int32 idx = 0; idx < input_size * input_size * input_size * 3;) samples_1[idx++] = strtod(points++, &points);
+	free(points_address);
 
 	uint32 size = (input_size > options.size) ? options.size : input_size;
 	if (input_size > 32) printf("ACR unsupports LUT>32, resampling enabled\n");
